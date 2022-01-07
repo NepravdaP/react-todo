4602e417bf52bd26c93686b01fd6131d186daa62
Pavel Harmaza <91322286+NepravdaP@users.noreply.github.com>
2022-01-07 23:44:19 +0300

    feat: add main structures and styles

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Pavel's Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
